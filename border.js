fcabf876ed99f9ac0f40171bb115a439f943ee54
PEEPOOE <96319418+PEEPOOE@users.noreply.github.com>
2022-04-24 18:25:05 +0100

    Add files via upload

---
+++ border.js
@@ -0,0 +1,15 @@
+let border = `
+<div class="top">
+buslightslookscary 
+<a href="/index.html">home</a> 
+<a href="/sillyreviews.html">sillyreviews</a>
+<a class="spoiler" href="/secret.html">secret</a>
+<marquee style="font-size:2em; background-color: blue;" behavior="scroll" direction="left" scrollamount="50">i beat ratchet and clank gladiator on exterminator mode in co op with famous mouse clicker kaiwala</marquee>
+</div>
+
+<div class="footer">
+<marquee style="font-size:2em; background-color: blue;" behavior="scroll" direction="right" scrollamount="20">
+<a href="https://www.youtube.com/c/buslightslookscary">hey</a>, <a href="https://mastodon.social/web/@verybadhouse">loser</a>
+</div>
+`;
+document.getElementById("border").innerHTML = border;
