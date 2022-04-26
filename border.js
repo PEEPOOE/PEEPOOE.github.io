dc8c33e6b250db69f3c64ff21d5c74706da155e1
PEEPOOE <96319418+PEEPOOE@users.noreply.github.com>
2022-04-26 11:23:24 +0100

    Add files via upload

--- border.js
+++ border.js
@@ -5,8 +5,8 @@ peepooe.github.io
 <a href="/2nd_page.html">2</a>
 <a href="/3rd_page.html">3</a>
 <a href="/4.html">4</a>
-<a href="/5.html">5</a>
-<a class="spoiler" href="/bruhv6.html">secret</a>
+<a href="/5.html">sec</a>
+<a class="spoiler" href="/bruhv6.html">ret</a>
 <marquee style="font-size:2em; background-color: blue;" behavior="scroll" direction="left" scrollamount="50">balls xd</marquee>
 </div>
 
